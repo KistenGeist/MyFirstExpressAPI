2f4581dbcb77a4763109711656d9f5f3af93861a
Wladislaw Kusnezow <wladislawkusnezow@web.de>
2022-12-02 12:10:46 +0100

    went back to just use app.listen, testet use of res.sendFile

--- Backend/main.js
+++ Backend/main.js
@@ -1,34 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8080;
-const path = require('path');
-const https = require("https");
-const fs = require("fs");
-const helmet = require("helmet");
+const port = 3000;
 const cors = require("cors");
 
-//Helmet security
-app.use(helmet());
-app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 //Cors
 app.use(cors());
-app.listen(port);
-
-// Add headers before the routes are defined
-app.use((req, res, next) => {
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // Pass to next layer of middleware
-    next();
-});
-
-app.all("/app/*", function (req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile("./public/app/index.html", {
-      root: __dirname,
-    });
-  });
 
 app.get('/identity', (req, res) => {
     res.status(200).send({
@@ -43,10 +20,10 @@ app.post('/identity/:id', (req, res) => {
     const { id } = req.params;
 
     //Get body (in JSON, for that see line 1-5) of API call
-    const { logo } = req.body;
+    const { logo } = req.body !== null ? req.body : {};
 
     if (!logo) {
-        res.status(418).send({ message: "Catch me if you can!" })
+        res.status(418).send({ error: "Catch me if you can!" })
     }
 
     res.send({
@@ -56,20 +33,17 @@ app.post('/identity/:id', (req, res) => {
 
 app.get('/WhatAreYou', (req, res) => {
     //res.send( {fs: "../Creeper.png"});
-    //res.sendFile('C:/Users/wkusnezow/Documents/GitHub/MyFirstAPI/Creeper.png');
+    try {
+        var dir = __dirname + "\\images\\Creeper.png";
+        res.sendFile(dir);
+        
+        //res.send({dirname: __dirname, dir: dir})
+    }
+    catch (err) {
+        res.send({error: err});
+    }
 });
 
-/**
- * Server create
- */
-
-/*
-https.createServer(
-    {
-        key: fs.readFileSync("./ssl/key.pem"),
-        cert: fs.readFileSync("./ssl/cert.pem")
-        //,passphrase: "d§$Fsdftr34rwefefdegfdwf", 
-    }
-    ,app
-).listen(port);
-*/
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
+});
