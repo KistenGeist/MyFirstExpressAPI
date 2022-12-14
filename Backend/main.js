dfe00eee822f55c3f66d004fd7e3bcd21e50533b
Wladislaw Kusnezow <wladislawkusnezow@web.de>
2022-12-14 13:21:54 +0100

    removed commetns

--- Backend/main.js
+++ Backend/main.js
@@ -116,34 +116,6 @@ app.get('/GetHaustiere', async (req, res) => {
 });
 
 app.get('/video', function (req, res) {
-    //method 1
-    /* try {
-        const range = req.headers.range;
-        if (!range) {
-            res.status(400).send("Requires Range Header");
-        }
-        const videoPath = __dirname + "\\TestVideo.mp4";
-        const videoSize = fs.statSync(__dirname + "\\TestVideo.mp4").size;
-        const CHUNK_SIZE = 10 ** 6; //1MB
-        const start = Number(range.replace(/\D/g, ""));
-        const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-        const contentLength = end - start + 1;
-    
-        const headers = {
-            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-            "Accept-Ranges": "bytes",
-            "Content-Length": contentLength,
-            "Content-Type": "video/mp4",
-        }
-        res.writeHead(206, headers); //partial content
-        const videoStream = fs.createReadStream(videoPath, { start, end });
-        videoStream.pipe(res);   
-    } catch (error) {
-       console.log(error) ;
-    } */
-    //var test = path.resolve("./TestVideo.mp4");
-
-    //method 2
     const readStream = fs.createReadStream(path.resolve(__dirname + "\\TestVideo.mp4"), { flags: "r" });
     res.header("Content-Type", "video/mp4");
     readStream.pipe(res);
--- Frontend/main.js
+++ Frontend/main.js
@@ -4,21 +4,6 @@ var btn = document.getElementById("btn");
 var loadVideo = document.getElementById("btnLoadVideo");
 var img = document.getElementById("img");
 var videoPlayer = document.getElementById("videoPlayer");
-var videoSource = document.createElement("source");
-videoSource.setAttribute('src', 'http://techslides.com/demos/sample-videos/small.mp4');
-videoSource.setAttribute("type", "video/mp4");
-
-videoPlayer.appendChild(videoSource);
-videoPlayer.play();
-
-setTimeout(function(){
-    videoPlayer.pause();
-
-    videoSource.setAttribute('src', 'http://techslides.com/demos/sample-videos/small.webm');
-    videoSource.setAttribute('type', 'video/webm');
-    videoPlayer.load();
-    videoPlayer.play();
-  }, 3000);
 
 btn.onclick = function () {
     $.ajax({
@@ -37,37 +22,6 @@ btn.onclick = function () {
 }
 
 loadVideo.onclick = function () {
-    /* $.ajax({
-        type: "GET",
-        //url: "http://localhost:8080/WhatAreYou",
-        url: "http://localhost:3000/video",
-        headers: {
-            Range: "bytes=0-10"
-        },
-        success: function (response) {
-            videoPlayer.src = response;
-            //console.log(response);
-        },
-        error: function (response, status) {
-            console.log(response);
-        }
-    }) */
-    /* .then((response) => response.body)
-    .then(async readableStream => {
-        const reader = readableStream.getReader();
-        let buffer = [];
-        while (1) {
-            const { value, done } = await reader.read();
-            if (done) {
-                const blob = new Blob(buffer);
-                const blobUrl = URL.createObjectURL(blob);
-                videoPlayer.src = blobUrl;
-                break;
-            }
-            buffer.push(value);
-        }
-    }) */
-    
     fetch("http://localhost:3000/video")
     .then((response) => response.body)
     .then(async readableStream => {
